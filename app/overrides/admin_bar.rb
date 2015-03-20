4b44ec0c38a8963ed3c3decd21b6ca822f877722
Quintin Adam <quintinadam@live.com>
2015-03-20 15:48:30 -0600

    moved bar to top

--- app/overrides/admin_bar.rb
+++ app/overrides/admin_bar.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: "spree/layouts/spree_application",
   name: "admin_bar",
-  insert_after: ".content-container",
+  insert_before: ".content-container",
   partial: "spree/admin_bar/admin_bar",
 )
